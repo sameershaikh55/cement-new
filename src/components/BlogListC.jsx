a89a62f1efc53ff1afd6b92ba93dc117081e6978
sameershaikh55 <sameermubeen4@gmail.com>
2022-05-24 15:37:14 +0500

    fixed

--- src/components/BlogListC.jsx
+++ src/components/BlogListC.jsx
@@ -5,69 +5,71 @@ import { imgUrl } from "../redux/config";
 const BlogListC = ({ data }) => {
   let history = useHistory();
 
+  data.sort(
+    (date1, date2) => new Date(date2.updated_at) - new Date(date1.updated_at)
+  );
+
   return (
     <div className="page_container mb-5 mt-3 mt-md-5">
       <div className="container-fluid">
         <div className="row">
           <div className="col-11 col-md-12 mx-auto">
             <div className="row">
-              {data
-                .map((prev, i) => {
-                  const {
-                    banner_img,
-                    banner_img_alt,
-                    banner_img_title,
-                    blog_title,
-                    meta_description,
-                    page_url,
-                  } = prev;
-                  return (
-                    <div
-                      data-aos="fade-up"
-                      data-aos-delay={`${
-                        (i === 0 && 1000) ||
-                        (i === 1 && 1100) ||
-                        (i === 2 && 1200) ||
-                        i * 100
-                      }`}
-                      data-aos-duration="400"
-                      data-aos-offset="600"
-                      key={i}
-                      className="col-12 col-md-6 col-lg-4 mt-5 mt-md-4"
-                    >
-                      <div className="blogListCard">
-                        <img
-                          className="w-100 rounded-3"
-                          src={imgUrl + banner_img}
-                          alt={banner_img_alt}
-                          title={banner_img_title}
-                        />
-                        <div className="my-3 px-3">
-                          <h6
-                            dangerouslySetInnerHTML={{ __html: blog_title }}
-                            className="fw-bold themeColorG text-uppercase"
-                          ></h6>
-                          <div
-                            dangerouslySetInnerHTML={{
-                              __html: meta_description,
-                            }}
-                            className="small"
-                          ></div>
-                          <button
-                            onClick={() => {
-                              history.push(`/blog/${page_url}`);
-                              window.scrollTo(0, 0);
-                            }}
-                            className="text-uppercase greenBtn px-4 py-1 mt-2 text-white"
-                          >
-                            know more
-                          </button>
-                        </div>
+              {data.map((prev, i) => {
+                const {
+                  banner_img,
+                  banner_img_alt,
+                  banner_img_title,
+                  blog_title,
+                  meta_description,
+                  page_url,
+                } = prev;
+                return (
+                  <div
+                    data-aos="fade-up"
+                    data-aos-delay={`${
+                      (i === 0 && 1000) ||
+                      (i === 1 && 1100) ||
+                      (i === 2 && 1200) ||
+                      i * 100
+                    }`}
+                    data-aos-duration="400"
+                    data-aos-offset="600"
+                    key={i}
+                    className="col-12 col-md-6 col-lg-4 mt-5 mt-md-4"
+                  >
+                    <div className="blogListCard">
+                      <img
+                        className="w-100 rounded-3"
+                        src={imgUrl + banner_img}
+                        alt={banner_img_alt}
+                        title={banner_img_title}
+                      />
+                      <div className="my-3 px-3">
+                        <h6
+                          dangerouslySetInnerHTML={{ __html: blog_title }}
+                          className="fw-bold themeColorG text-uppercase"
+                        ></h6>
+                        <div
+                          dangerouslySetInnerHTML={{
+                            __html: meta_description,
+                          }}
+                          className="small"
+                        ></div>
+                        <button
+                          onClick={() => {
+                            history.push(`/blog/${page_url}`);
+                            window.scrollTo(0, 0);
+                          }}
+                          className="text-uppercase greenBtn px-4 py-1 mt-2 text-white"
+                        >
+                          know more
+                        </button>
                       </div>
                     </div>
-                  );
-                })
-                .reverse()}
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
